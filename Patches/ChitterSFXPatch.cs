f3ea8adf73cc585b2f152113cf27dffe5fafb5ed
GonkUnit <szymcyb04@gmail.com>
2024-02-09 17:59:28 +0000

    Add project files.

---
+++ Patches/ChitterSFXPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GameNetcodeStuff;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImWalkingHereMod.Patches
+{
+    [HarmonyPatch(typeof(HoarderBugAI))]
+    internal class ChitterSFXPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+
+        static void OverrideAudio(HoarderBugAI __instance)
+        {
+            __instance.chitterSFX = ImWalkingHereBase.SoundFX.ToArray();
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,61 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using ImWalkingHereMod.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ImWalkingHereMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ImWalkingHereBase : BaseUnityPlugin
+    {
+        private const string modGUID = "LegoStego.ImWalkingHereMod";
+        private const string modName = "Im Walking Here Mod";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ImWalkingHereBase Instance;
+
+        internal ManualLogSource mls;
+
+        internal static List<AudioClip> SoundFX;
+        internal static AssetBundle Bundle;
+
+        void Awake()
+        {
+            if(Instance == null) 
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("The ImWalkingHereMod has awaken");
+
+            harmony.PatchAll(typeof(ChitterSFXPatch));
+
+            mls = Logger;
+
+            SoundFX = new List<AudioClip>();
+            string folderLocation = Instance.Info.Location;
+            folderLocation = folderLocation.TrimEnd("ImWalkingHereMod.dll".ToCharArray());
+            Bundle = AssetBundle.LoadFromFile(folderLocation + "imwalkinghere");
+
+            if(Bundle != null)
+            {
+                mls.LogInfo("Successfully loaded asset bundle");
+                SoundFX = Bundle.LoadAllAssets<AudioClip>().ToList();
+            }
+            else
+            {
+                mls.LogError("Failed to load asset bundle");
+            }
+        }
+    }
+}
